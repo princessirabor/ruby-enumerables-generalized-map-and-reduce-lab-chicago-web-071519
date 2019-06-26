9f391527e99cec2baea6f6484d97dcd7c705b802
Princess Irabor <irabor.princess1@gmail.com>
2019-06-26 19:02:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,6 +10,7 @@ end
 def reduce(source_array,start_val = 0 )
   i = 0;
   value = start_val
+  p "#{start_val}"
   while i < source_array.length do 
     if source_array[i] == nil
       source_array[i] = !!source_array[i]
@@ -17,13 +18,5 @@ def reduce(source_array,start_val = 0 )
     value = yield(source_array[i],value)
     i=i+1
   end
- # if value == nil
- #  value = false
- # end
   value
 end
-#reduce(source_array) do |index, n|
-#  index && n
-#end
-
-#map([1, 2, 3, -9]){|n| n * n}
