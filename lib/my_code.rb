78a69a98cc0e93edb9137ff676277a8c38b8dcdd
Princess Irabor <irabor.princess1@gmail.com>
2019-06-26 18:07:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
 # Your Code Here
+def map(source_array)
+  new =[]
+  source_array.length.times do |index|
+    new[index] = yield (source_array[index])
+  end
+  new
+end
+
+def reduce(source_array,start_val = 0 )
+  source_array.length.times do |index|
+     yield (source_array[index])
+  end
+end
+#map(source_array) do |index|
+#  index *(-1)
+#end
+
+#map([1, 2, 3, -9]){|n| n * n}
