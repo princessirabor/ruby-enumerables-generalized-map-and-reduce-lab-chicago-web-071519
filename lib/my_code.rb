2649e1ed9367a0cc5a81db503638c322e01fe596
Princess Irabor <irabor.princess1@gmail.com>
2019-06-26 19:30:10 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # Your Code Here
+def map(source_array)
+  new =[]
+  source_array.length.times do |index|
+    new[index] = yield (source_array[index])
+  end
+  new
+end
+
+def reduce(source_array,start_val = 0 )
+  i = 0;
+  value = start_val
+  while i < source_array.length do 
+    if source_array[i] == nil
+      source_array[i] = !!source_array[i]
+    end
+    value = yield(source_array[i],value)
+    p "#{value}"
+    i=i+1
+  end
+  value
+end
